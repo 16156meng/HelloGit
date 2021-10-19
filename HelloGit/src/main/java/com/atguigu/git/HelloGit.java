0ef1bd2c5bd3d6be2f27cc8d1061e3d4121341d0
16156meng <18766236080@163.com>
2021-10-19 11:39:28 +0800

    添加文件

---
+++ HelloGit/src/main/java/com/atguigu/git/HelloGit.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author Meng
+ * @create 2021-10-19  11:28
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("'Hello Git !");
+    }
+}
