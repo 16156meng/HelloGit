d66241700c1cf04fc4e611502fc94583f483348d
16156meng <18766236080@163.com>
2021-10-19 13:52:17 +0800

    创建分支

--- HelloGit/src/main/java/com/atguigu/git/HelloGit.java
+++ HelloGit/src/main/java/com/atguigu/git/HelloGit.java
@@ -1,11 +1,8 @@
 package com.atguigu.git;
 
-/**
- * @author Meng
- * @create 2021-10-19  11:28
- */
 public class HelloGit {
     public static void main(String[] args) {
-        System.out.println("'Hello Git !");
+        System.out.println("Hello Git !");
+        System.out.println("创建分支!");
     }
 }
